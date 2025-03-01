b8a9716a80b4c3263ecab972f54440b679768bf6
EstevaoLocks <estevao.locks11@gmail.com>
2025-03-01 07:57:06 -0300

    resolucao exercicio3 da aula 2

--- aulas/exercicios_aula02/exercicio03.php
+++ aulas/exercicios_aula02/exercicio03.php
@@ -1,5 +1,16 @@
 <?php
-
+    $n1 = 7;
+    $n2 = 3;
+    $n1 += $n2;
+    echo "A soma de 3 + 7 é igual a $n1";
+    $n1 -= $n2;
+    echo "A subtração de 3 - 7 é igual a $n1";
+    $n1 *= $n2;
+    echo "A multiplicação de 3 * 7 é igual a $n1";
+    $n1 := $n2;
+    echo "A divisão de  3 : 7 é igual a $n1";
+    $n1 %= $n2;
+    echo "O resto da divisão de  3 : 7 é igual a $n1";
 
 
 
