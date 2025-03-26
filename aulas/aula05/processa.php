176f69d0504ff73ff4b5247964322293a97479ed
EstevaoLocks <estevao.locks11@gmail.com>
2025-03-26 18:38:50 -0300

    salvando

--- aulas/aula05/processa.php
+++ aulas/aula05/processa.php
@@ -1,10 +1,11 @@
 <?php
     $email = $_POST['email'];
     $senha = $_POST['senha'];
+    $nome = "Aluno";
 
     if ($email == "aluno@email.com" && $senha == "1234"){
         //manda para autorizado.php
-        header('Location: painel/autorizado.html');
+        header('Location: painel/autorizado.php?nome='.$nome);
         //echo "AUTORIZADO";
     }else{
         //manda para nao açutorizado.php
