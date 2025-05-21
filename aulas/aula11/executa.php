d597ee614e096eb2c0de503c0e8b9d25bc175ca5
EstevaoLocks <estevao.locks11@gmail.com>
2025-05-21 12:43:24 -0300

    salvando

--- aulas/aula11/executa.php
+++ aulas/aula11/executa.php
@@ -1,5 +1,13 @@
 <?php
-    
+    include_once 'Produto.class.php';
+
+    $produto1 = new Produto("caneta", 9.90, 20);
+
+    $produto1->mostrarDetalhes();
+    $produto1->setNome("borracha");
+    echo "Novo nome: {$produto1->getNome()}";
+    $produto1->setPreco(5.50);
+    echo "Novo preço: R${$produto1->getPreco()}";
 
 
 ?>
