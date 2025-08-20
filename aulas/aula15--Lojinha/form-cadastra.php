a38924e93c67c91346a10074b7e9bbb732788604
EstevaoLocks <estevao.locks11@gmail.com>
2025-08-20 12:23:38 -0300

    salvando

--- aulas/aula15--Lojinha/form-cadastra.php
+++ aulas/aula15--Lojinha/form-cadastra.php
@@ -3,9 +3,17 @@
 ?>
 
     <div class="container">
-        <form>
+        <h2>Cadastro de Produtos</h2>
+
+        <form action="inserir.php" method="POST">
             <div class="mb-3">
                 <input type="text" class="form-control" name="nome" placeholder="Digite o nome do produto">
+            </div>
+            <div class="mb-3">
+                <input type="decimal" class="form-control" name="preco" placeholder="Digite o preço do produto">
+            </div>
+            <div class="mb-3">
+                <input type="number" class="form-control" name="qnt" placeholder="Digite a quantidade do produto">
             </div>
 
             <button type="submit" class="btn btn-primary">Submit</button>
--- aulas/aula15--Lojinha/index.php
+++ aulas/aula15--Lojinha/index.php
@@ -3,9 +3,9 @@
 ?>
 
     <!-- Botões navegação -->
-    <div class="container">
-        <a href="form-cadastra.php" type="button" class="btn btn-outline-success">Cadastrar</a>
-        <a href="listar.php" type="button" class="btn btn-outline-info">Listar</a>
+    <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+        <a href="form-cadastra.php" type="button" class="btn btn-danger">Cadastrar Produto</a>
+        <a href="listar.php" type="button" class="btn btn-warning">Lista de Produtos</a>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
