67f2c81285d3e5f54e008969093e7c315dc9bc16
EstevaoLocks <estevao.locks11@gmail.com>
2025-08-20 12:07:30 -0300

    dividindo index com 'pedaco.php' e add form

--- aulas/aula15--Lojinha/form-cadastra.php
+++ aulas/aula15--Lojinha/form-cadastra.php
@@ -1,11 +1,17 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastrar Item</title>
-</head>
-<body>
+<?php
+    include 'pedaco.php';
+?>
+
+    <div class="container">
+        <form>
+            <div class="mb-3">
+                <input type="text" class="form-control" name="nome" placeholder="Digite o nome do produto">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+
+        </form>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
 </body>
--- aulas/aula15--Lojinha/index.php
+++ aulas/aula15--Lojinha/index.php
@@ -1,21 +1,11 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-    <title>Lojinha Etec MCM</title>
-</head>
-<body>
-    <div class="container">
-        <h1>SISTEMA CRUD - LOJA</h1>
-        <h2>Aluno: Estevão Oliveira Locks</h2>
-    </div>
+<?php
+    include 'pedaco.php';
+?>
 
     <!-- Botões navegação -->
     <div class="container">
-        <a href="form-cadastra.php" type="button" class="btn btn-outline-success">Success</a>
-        <a href="listar.php" type="button" class="btn btn-outline-info">Info</a>
+        <a href="form-cadastra.php" type="button" class="btn btn-outline-success">Cadastrar</a>
+        <a href="listar.php" type="button" class="btn btn-outline-info">Listar</a>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
